a888c97ca46c3102bf1d1933304785839c1c24e5
Daniel Cruz <danielc@unb.br>
2020-04-11 23:38:15 -0300

    Incrementando aliases com funções

--- .user_config/aliases.sh
+++ .user_config/aliases.sh
@@ -24,8 +24,23 @@ git_patch() {
     elif [ "$1" == 'sync' ];
     then
         echo -e "\033[01;34mChecking for changes on remote repo...\033[00m"; command git pull; echo -e "\033[01;34mTrying to push local changes into remote...\033[00m"; command git push
+    elif [ "$1" == "new-project" ]
+    then
+        if [ -z "$2" ]
+        then
+            echo "É necessário indicar o nome da pasta do projeto a ser criado...";
+        else
+            mkdir "$2"
+            cd "$2"
+            nano README.md
+            command git init
+            command git add README.md
+            command git commit -m "Registro inicial"
+            command git remote add origin "git@github.com:bwb0de/$2.git"
+            command git push -u origin master
+        fi
     else
-        command git $@
+        command git "$@"
     fi
 }
 
@@ -38,11 +53,217 @@ apt_patch() {
         echo -e "$(apt --version) » $INFO «"
         cat /tmp/apt_help_init
         echo -e "\033[01;34m  installed - list all installed packages\033[00m"
+        echo -e "\033[01;34m  download-installed - downloads all installed packages to current folder\033[00m"
         cat /tmp/apt_help_end
     elif [ "$1" == 'installed' ];
     then
         command apt list | grep "\[installed" | cut -d"/" -f1
+    elif [ "$1" == 'download-installed' ];
+    then
+        command apt-get download $(apt installed)
     else
-        command apt $@
+        command apt "$@"
     fi
+}
+
+
+speedscrap () {
+    if ! [ -d ~/.teller ]
+    then
+        command mkdir -p ~/.teller
+    fi
+
+    if [ $# -eq 0 ]
+    then
+        echo "É necessário fornecer argumentos para o programa... "
+        echo ""
+        echo " Uso: speedscrap <filename> [-t|-c|-g] <texto>"
+        echo ""
+        echo " -t    Escreve o <texto> no arquivo <filename>"
+        echo " -c    Cria um contador em <filename>. Se já existir, incrementa o valor registrado em 1"
+        echo " -g    Lê o conteúdo corrente gravado em <filename>."
+        echo ""
+    else
+
+        if [ "$2" == "-t" ]
+        then
+            echo "$3" > "~/.teller/$1"
+
+        elif [ "$2" == "-c" ]
+        then
+            expr 0 + $(cat "~/.teller/$1") + 1 > "~/.teller/$1"
+
+        elif [ "$2" == "-g" ]
+        then
+            cat "~/.teller/$1"
+        fi
+    fi
+}
+
+
+showip () {
+    wget http://ipinfo.io/ip -qO -    
+}
+
+
+save-command-history() {
+    cat < ~/.bash_history > /tmp/cmd_get_data
+    while read p;
+    do
+        LINE=$(echo $p | awk -F" " '{ print $0 }')
+        CMD_NAME=$(echo $p | awk -F" " '{ print $1 }')
+        echo "$LINE" >> "~/Documentos/Devel/Terminal Linux/$CMD_NAME"
+    done < /tmp/cmd_get_data
+}
+
+
+mkdec () {
+    if [ $# -eq 0 ]
+    then
+        echo "Forma de uso:"
+        echo "decmaker <perfil>"
+    else
+        if ! [ -d ~/.decmaker ]
+        then
+            command mkdir -p ~/.decmaker
+        fi
+        cp skell1.odt tmp/
+        cd tmp/
+        unzip skell1.odt
+        rm skell1.odt
+        cat < content.xml | sed "s/\[§NOME§\]/$1/g" > /tmp/content.xml
+        cat < /tmp/content.xml | sed "s/\[§STATUS§\]/$2/g" > content.xml
+        cat < content.xml | sed "s/\[§TEMPO§\]/$3/g" > /tmp/content.xml
+        cat < /tmp/content.xml | sed "s/\[§PERIODO§\]/$4/g" > content.xml
+        cat < content.xml | sed "s/\[§DATA§\]/$5/g" > /tmp/content.xml
+        mv /tmp/content.xml content.xml
+        zip -0mr skell1.odt *
+    fi
+}
+
+sshcp () {
+    if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]
+    then
+        echo "Faltam argumentos..."
+        echo ""
+        echo "Forma de uso:"
+        echo "sshcp <user> <ip> <source_folder> <destination_folder>"
+        echo ""
+    else
+        rsync -urlHpog --safe-links -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress "$3" "$1@$2:$4"
+    fi
+}
+
+
+get-bing-wallpaper () {
+    IMGFOLDER=~/
+    t="$(ping -c 1 www.google.de | sed -ne '/.*time=/{;s///;s/\..*//;p;}')"
+    wget https://www.bing.com/HPImageArchive.aspx?format=xml\&idx=0\&n=1\&mkt=de-DE -O /tmp/download.xml
+    imgpath=$(sed -n -e 's/.*<url>\(.*\)<\/url>.*/\1/p' /tmp/download.xml)
+    imgsrc='https://www.bing.com'${imgpath//1366x768/1920x1080}
+    file=$(basename "$imgsrc")
+    filename="${file%.*}"
+    extension="${file##*.}"
+    wget $imgsrc -O $IMGFOLDER/$(date -d "today" +"%Y_%m_%d")_$filename.$extension
+    fileurl=Image=file://$IMGFOLDER/$(date -d "today" +"%Y_%m_%d")_$filename.$extension
+}
+
+
+img-batch-resize () {
+
+    iJPG=$(ls | grep .jpg | wc -l)
+    iJPEG=$(ls | grep .jpg | wc -l)
+    iPNG=$(ls | grep .png | wc -l)
+    quantidade_imagens=$(expr $iJPG + $iJEPG + $iPNG)
+    info="Nenhuma imagem foi encontrada da pasta..."
+
+
+    if [ $1 ] || [ $2 ];
+    then
+        case "$1" in
+            "-h" | "--help")
+            echo "Forma de uso:"
+            echo "img-batch-resize -t "
+            echo "img-batch-resize -r <size> "
+            echo ""
+            echo "Opções:"
+            echo " -r, --resize       Redimensiona os arquivos conforme o valor indicado"
+            echo " -t                 Gera arquivos pequenos (thumbnail) das imagens com [160px]"
+            echo ""
+            ;;
+
+            "-r" | "--resize")
+            if [ $2 ] ;
+            then
+                if [ $quantidade_imagens -eq 0 ]
+                then 
+                    echo "img-batch-resize: $info" ; 
+                fi
+                
+                if ! [ $iJPG -eq 0 ] ;
+                then
+                    for i in *.jpg ;
+                    do
+                        convert -resize "$2" "$i" "resized_$2-$i" ; 
+                    done
+                fi
+                
+                if ! [ $iJPEG -eq 0 ] ;
+                then
+                    for i in *.jpeg ;
+                    do
+                        convert -resize "$2" "$i" "resized_$2-$i" ; 
+                    done
+                fi
+
+
+                if ! [ $iPNG -eq 0 ] ; 
+                then
+                    for i in *.png ; 
+                    do 
+                        convert -resize "$2" "$i" "resized_$2-$i" ; 
+                    done
+                fi
+                
+            else
+                img-batch-resize -h
+            fi
+            ;;
+
+            "-t" | "--thumbnail")
+            if [ $quantidade_imagens -eq 0 ]
+            then
+                echo "img-batch-resize: $info" ; 
+            fi
+            
+            if ! [ $iJPG -eq 0 ] ; 
+            then
+                for i in *.jpg ; 
+                do 
+                    convert -resize 160 "$i" "thumbnail-$i" ; 
+                done ; 
+            fi
+            
+            if ! [ $iJPEG -eq 0 ] ; 
+            then
+                for i in *.jpeg ; 
+                do 
+                    convert -resize 160 "$i" "thumbnail-$i" ; 
+                done ; 
+            fi
+
+            if ! [ $iPNG -eq 0 ] ; 
+            then
+                for i in *.png ; 
+                do 
+                    convert -resize 160 "$i" "thumbnail-$i" ; 
+                done ; 
+            fi
+            ;;
+
+        esac
+    else
+        img-batch-resize -h
+    fi
+
 }
